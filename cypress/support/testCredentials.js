0307710bd94cc018e00bf45a12aa8925ea3b744f
Thea <theola51924@stud.noroff.no>
2024-04-25 15:03:11 +0200

    added test credentials for the cypress tests

---
+++ cypress/support/testCredentials.js
@@ -0,0 +1,8 @@
+export const testUrl = "/";
+
+export const validEmail = Cypress.env("USER_EMAIL");
+export const validPassword = Cypress.env("USER_PASSWORD");
+export const validUsername = Cypress.env("USER_NAME");
+
+export const invalidEmail = "invalidEmail@noroff.no";
+export const invalidPassword = "invalidPassword";
