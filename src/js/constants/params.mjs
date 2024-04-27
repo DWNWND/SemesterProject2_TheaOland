147ea794d3f8181efa3e04950d2e1c55f30c02cc
Thea <theola51924@stud.noroff.no>
2024-04-27 13:00:53 +0200

    added params file with all the api params

---
+++ src/js/constants/params.mjs
@@ -0,0 +1,9 @@
+export const PARAM_seller = "_seller"; //boolean
+export const PARAM_bids = "_bids"; //boolean
+export const PARAM_activeListings = "_active"; //boolean
+export const PARAM_listingTags = "_tag"; //string
+export const PARAM_associatedListing = "_listing";
+export const PARAM_associatedWins = "_wins";
+
+// example:
+// /auction/listings?_tag=my_tag&_active=true
