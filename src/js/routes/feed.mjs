4852178357a07f68f5578c24e5184234be4b4f89
Thea <theola51924@stud.noroff.no>
2024-04-28 17:54:44 +0200

    adjusted the feed functionality to work with logged in or logged out users, a load more btn, a display for error messages and a loader

--- src/js/routes/feed.mjs
+++ src/js/routes/feed.mjs
@@ -1,44 +1,54 @@
 import { get } from "../api/listings/get.mjs";
+import { loadMoreListings } from "../events/listners/nextPage.mjs";
+import { load } from "../storage/load.mjs";
 
 const feed = document.getElementById("feed");
+const loadMoreBtn = document.getElementById("loadMore");
+const loader = document.getElementById("loader");
+const userFeedback = document.getElementById("userFeedback");
+const uxElement = document.getElementById("uxElement");
+const searchElement = document.getElementById("searchElement");
+const paginationElement = document.getElementById("paginationElement");
+let page = 1;
 
-export async function loggedOutFeed() {
-  const listings = await get("listingsByPage", 1);
+export async function generateFeed() {
+  const listings = await get("listingsByPage", page);
   renderListings(listings, feed);
+  loadMoreListings(loadMoreBtn);
 }
 
-export function loggedInFeed() {}
-
-//ARRAY OF LISTINGS
+//ARRAY OF LISTINGS (USING LISTING TEMPLATE)
 export function renderListings(listingsArray, container) {
   if (listingsArray.length === 0 || !listingsArray) {
     console.log("no listings found, check renderListings function in the feed.js");
-    // loader.style.display = "none";
-    // loadMoreBtn.style.display = "none";
-    // displayMessage.innerText = "there's no posts matching this search or filter";
+    loader.style.display = "none";
+    userFeedback.innerText = "there's no listings matching this search.";
   } else {
-    // displayMessage.innerText = "";
+    const token = load("token");
     for (let i = 0; i < listingsArray.length; i++) {
-      container.append(loggedOutListingTemplate(listingsArray[i]));
+      container.append(listingTemplate(listingsArray[i], token));
     }
-    // loader.style.display = "none";
+    uxElement.innerHTML = "";
+    searchElement.style.display = "block";
+    paginationElement.style.display = "block";
+    loadMoreBtn.style.display = "block";
   }
 }
 
-//ONE LISTING
-export function renderListing(listing, container) {
-  container.append(loggedOutListingTemplate(listing));
+//ONE LISTING - NOT IN USE YET
+export function renderSingleListing(listing, container) {
+  container.append(listingSomething(listing));
 }
 
-//TEMPLATE LOGGED OUT START FEED
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-export function loggedOutListingTemplate(listingData) {
+// ALL LISTINGS TEMPLATE
+export function listingTemplate(listingData, userIsLoggedIn) {
   const listingMedia = listingData.media.length;
   const listingTitle = listingData.title;
-  const listingEndsAt = new Date(listingData.endsAt);
-  const listingEndsAtDate = listingEndsAt.toLocaleDateString();
-  const listingEndsAtTime = listingEndsAt.toLocaleTimeString();
   const listingBids = listingData.bids.length;
+  //Inspired by: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+  const listingEndsAt = new Date(listingData.endsAt);
+  const listingEndsAtDate = listingEndsAt.toLocaleDateString(); //NOTE: consider adding this or adjust design
+  const listingEndsAtTime = listingEndsAt.toLocaleTimeString();
 
   const col = document.createElement("div");
   col.classList.add("col");
@@ -49,6 +59,7 @@ export function loggedOutListingTemplate(listingData) {
   const mainImg = document.createElement("img");
   mainImg.classList.add("object-fit-cover", "main-listing-img");
 
+  //check to see if theres images and alt-text connected to the listing, and if not use placeholders
   if (listingMedia > 0) {
     const listingPhoto = listingData.media[0].url;
     const listingDescription = listingData.media[0].alt;
@@ -80,23 +91,54 @@ export function loggedOutListingTemplate(listingData) {
   const currentBid = document.createElement("div");
   currentBid.classList.add("current-bid");
 
-  //https://flexiple.com/javascript/get-last-array-element-javascript
+  //check to see if theres bids on the listing, and if there is, display the last bid
   if (listingBids > 0) {
     const allBids = listingData.bids;
+    //Inspired by: https://flexiple.com/javascript/get-last-array-element-javascript
     const lastBid = allBids[allBids.length - 1];
     currentBid.innerText = lastBid.amount + " $";
   }
   if (listingBids === 0) {
-    currentBid.innerText = "0 $"; //add a special styling for the ones without bids
+    currentBid.innerText = "0 $"; //NOTE: add a special styling for the ones without bids??
   }
 
   const bidTimer = document.createElement("div");
   bidTimer.classList.add("timer");
   bidTimer.innerText = listingEndsAtTime;
 
-  bidContainer.append(currentBid, bidTimer);
-  listing.append(mainImg, titleContainer, bidContainer);
-  col.append(listing);
+  //LISTINGS DISPLAYED PUBLICLY (NOT LOGGED IN)
+  if (!userIsLoggedIn) {
+    console.log("theres no one loggedin");
+    bidContainer.append(currentBid, bidTimer);
+    listing.append(mainImg, titleContainer, bidContainer);
+    col.append(listing);
+  }
 
+  //LISTINGS DISPLAYED (LOGGED IN)
+  if (userIsLoggedIn) {
+    const listingFooter = document.createElement("div");
+    listingFooter.classList.add("listing-footer", "d-flex", "flex-column", "gap-2");
+
+    const viewListingBtn = document.createElement("button");
+    viewListingBtn.classList.add("btn-local", "btn-height-s", "btn-width-100", "btn-white-black", "btn-fontsize-m", "uppercase");
+    viewListingBtn.setAttribute("href", "#");
+    viewListingBtn.innerText = "View";
+
+    bidContainer.append(currentBid, bidTimer);
+    listingFooter.append(bidContainer, viewListingBtn);
+    listing.append(mainImg, titleContainer, listingFooter);
+    col.append(listing);
+
+    //LISTINGS BY USER SPESIFIC
+    const pathname = window.location.pathname;
+    if (pathname.includes("user")) {
+      const editListingBtn = document.createElement("button");
+      editListingBtn.classList.add("btn-local", "btn-height-s", "btn-width-100", "btn-white-black", "btn-fontsize-m", "uppercase");
+      editListingBtn.setAttribute("href", "#");
+      editListingBtn.innerText = "Edit";
+
+      listingFooter.append(editListingBtn);
+    }
+  }
   return col;
 }
