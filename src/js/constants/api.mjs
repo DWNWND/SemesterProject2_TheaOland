8114e9fb4a9f2b8ddf257acb9735619c465ca7b9
Thea <theola51924@stud.noroff.no>
2024-04-28 22:15:08 +0200

    updated the api url for auth functionality

--- src/js/constants/api.mjs
+++ src/js/constants/api.mjs
@@ -1,7 +1,7 @@
 // export const API_KEY = process.env.USER_PASSWORD;
 export const API_BASE = "https://v2.api.noroff.dev";
-export const API_AUTH = "/auth";
-export const API_REGISTER = "/register";
-export const API_LOGIN = "/login";
+// export const API_AUTH = "/auth";
+export const API_REGISTER = "/auth/register";
+export const API_LOGIN = "/auth/login";
 export const API_LISTINGS = API_BASE + `/auction/listings/`;
 export const API_PROFILES = API_BASE + `/auction/profiles/`;
