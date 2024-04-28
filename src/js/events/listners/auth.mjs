c575287ec07cb7feef54928e746106e1642c40ec
Thea <theola51924@stud.noroff.no>
2024-04-28 22:20:11 +0200

    added login and register authentication and evenlisteners listnening for login and register

---
+++ src/js/events/listners/auth.mjs
@@ -0,0 +1,35 @@
+import { userFeedback } from "../../ui/components/errors/userFeedback.mjs";
+import { register, login } from "../../api/auth/index.mjs";
+
+export async function loginAuth(event) {
+  event.preventDefault();
+
+  const email = event.target.loginEmail.value;
+  const password = event.target.loginPassword.value;
+
+  await login(email, password);
+}
+
+export async function registerAuth(event) {
+  event.preventDefault();
+
+  const name = event.target.registerUsername.value;
+  const email = event.target.registerEmail.value;
+  const firstPassword = event.target.registerPassword.value;
+  const passwordRepeat = event.target.registerRepeatPassword.value;
+
+  if (!name || !email || !firstPassword || !passwordRepeat) {
+    errorMessage = "Please fill in all the registration fields.";
+    userFeedback(errorMessage, generalErrorContainer);
+  } else {
+    validatePassword(firstPassword, passwordRepeat);
+    if (passwordRepeat === firstPassword) {
+      await register(name, email, firstPassword);
+    }
+  }
+}
+
+export function authentication() {
+  document.forms.loginForm.addEventListener("submit", loginAuth);
+  document.forms.registerForm.addEventListener("submit", registerAuth);
+}
