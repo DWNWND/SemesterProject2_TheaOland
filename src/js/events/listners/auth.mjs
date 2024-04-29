39fec3655a900c71d321ee3ea6c9e18cc97d4b2c
Thea <theola51924@stud.noroff.no>
2024-04-29 20:46:39 +0200

    added userFeedback

--- src/js/events/listners/auth.mjs
+++ src/js/events/listners/auth.mjs
@@ -17,10 +17,11 @@ export async function registerAuth(event) {
   const email = event.target.registerEmail.value;
   const firstPassword = event.target.registerPassword.value;
   const passwordRepeat = event.target.registerRepeatPassword.value;
+  const errorContainer = document.getElementById("userFeedback");
 
   if (!name || !email || !firstPassword || !passwordRepeat) {
     errorMessage = "Please fill in all the registration fields.";
-    userFeedback(errorMessage, generalErrorContainer);
+    userFeedback(errorMessage, errorContainer);
   } else {
     validatePassword(firstPassword, passwordRepeat);
     if (passwordRepeat === firstPassword) {
