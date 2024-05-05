9b499780f297d62a72a6d82af83cdba14604dbc0
Thea <theola51924@stud.noroff.no>
2024-05-05 20:31:21 +0200

    cleaned up the register function to align with the new login function

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -3,31 +3,28 @@ import { API_BASE, API_REGISTER } from "../../constants/index.js";
 import { userFeedback } from "../../ui/components/errors/userFeedback.js";
 import { login } from "./index.js";
 
-let errorMessage;
+let feedbackMessage;
+const errorContainer = document.getElementById("userFeedback");
 
 export async function register(name, email, password) {
-  const errorContainer = document.getElementById("userFeedback");
+  const url = API_BASE + API_REGISTER;
+  const response = await callApiWith(url, {
+    method: "POST",
+    body: JSON.stringify({ name, email, password }),
+  });
 
-  try {
-    const url = API_BASE + API_REGISTER;
-    const response = await callApiWith(url, {
-      method: "POST",
-      body: JSON.stringify({ name, email, password }),
-    });
+  if (response.status === 201) {
+    login(email, password);
+    return;
+  }
 
-    if (response.status === 201) {
-      login(email, password);
-    }
-
-    if (response.status === 400) {
-      errorMessage = "There is already an account with these credentials, try logging in instead";
-      throw new Error("There is already an account with these credentials");
-    } else if (response.status >= 401) {
-      errorMessage = "An unexpected error occured, please try again later";
-      throw new Error("Unknown error - from register function");
-    }
-  } catch (error) {
-    userFeedback(errorMessage, errorContainer);
-    console.log(error);
+  if (response.status === 400) {
+    feedbackMessage = "There is already an account with these credentials, try logging in instead";
+    userFeedback(feedbackMessage, errorContainer);
+    throw new Error("There is already an account with these credentials");
+  } else if (response.status >= 401) {
+    feedbackMessage = "An unexpected error occured, please try again later";
+    userFeedback(feedbackMessage, errorContainer);
+    throw new Error("Unknown error - from register function");
   }
 }
