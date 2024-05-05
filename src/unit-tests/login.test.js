1580a5e557db3229df5c8600f68df2f8cf5c9f20
Thea <theola51924@stud.noroff.no>
2024-05-05 20:36:26 +0200

    cleaned up and reworked the login tests to work with JSDOM and to await the login funciton correctly when looking for a throw

--- src/unit-tests/login.test.js
+++ src/unit-tests/login.test.js
@@ -1,43 +1,66 @@
-//The login function stores a token when provided with valid credentials
-
-import { login } from "../js/api/auth/index.js";
-
-//MOCK: localStorage
-const localStorageMock = {
-  setItem: jest.fn(),
-};
-global.localStorage = localStorageMock;
+import { login } from "../js/api/auth/login.js";
 
 //MOCK: successful fetch request
 const mockFetchSuccess = jest.fn().mockResolvedValue({
   ok: true,
+  status: 200,
   json: async () => ({
-    accessToken: "accessTokenMock",
+    data: {
+      accessToken: "accessToken",
+      name: "username",
+      email: "email",
+    },
   }),
 });
 
-//MOCK: failed fetch request
+//MOCK: failed fetch request 401
 const mockFetchFailure = jest.fn().mockResolvedValue({
   ok: false,
-  statusText: "Unauthorized",
+  status: 401,
 });
+
+// //MOCK: localStorage
+const localStorageMock = {
+  setItem: jest.fn(),
+  getItem: jest.fn(),
+};
+global.localStorage = localStorageMock;
 
 describe("login function", () => {
   it("stores a token when provided with valid credentials", async () => {
-    //Mock
     global.fetch = mockFetchSuccess;
-
-    //Assertion
     await login("validEmail", "validPassword");
-    expect(localStorage.setItem).toHaveBeenCalledWith("token", JSON.stringify("accessTokenMock"));
-  });
-  // it("throws an error if provided with unvalid credentials", async () => {
-  //   //Mock
-  //   global.fetch = mockFetchFailure;
 
-  //   //Assertion
-  //   await expect(async () => {
-  //     await login("invalidEmail", "invalidPassword");
-  //   }).rejects.toThrow("Unauthorized");
-  // });
+    // Assertions
+    expect(fetch).toHaveBeenCalled();
+    expect(localStorage.setItem).toHaveBeenCalledWith("token", JSON.stringify("accessToken"));
+    expect(localStorage.getItem).toHaveBeenCalledWith("token");
+  });
+  it("throws an error when calling login with invalid credentials", async () => {
+    global.fetch = mockFetchFailure;
+
+    // Assertions
+    await expect(login("invalidEmail", "invalidPassword")).rejects.toThrow("Email and/or password does not match.");
+  });
 });
+
+//OLD
+// it("calls the API and throws an error", async () => {
+//   const mockFetchFailure = jest.fn().mockResolvedValue({
+//     ok: false,
+//     status: 401,
+//     // statusText: "Email and/or password does not match.",
+//   });
+
+//   //Mock
+//   global.fetch = mockFetchFailure;
+
+//   expect.assertions(2);
+//   try {
+//     await login("invalidEmail", "invalidPassword");
+//   } catch (error) {
+//     expect(error.name).toEqual("Error");
+//     expect(error).toEqual("Email and/or password does not match.");
+//     // expect(fetch.status).toEqual(401);
+//   }
+// });
