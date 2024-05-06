d14140d790178bee1e5ae5ee5a15d41e799e0470
Thea <theola51924@stud.noroff.no>
2024-05-06 19:52:47 +0200

    updated the console.log pathnames for readability while in dev

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,30 @@
 async function router() {
   const pathname = window.location.pathname;
-  console.log(pathname.toLowerCase());
 
   if (pathname.toLowerCase() === "/semesterproject2_theaoland/" || pathname === "/") {
-    console.log("Home page/Feed");
+    console.log("Pathname: ", pathname, ", Location: Home page/Feed");
     const feedRequest = "./routes/feed.js";
     const { generateFeed } = await import(feedRequest);
     await generateFeed();
     return;
   }
   if (pathname.toLowerCase().includes("/auth/")) {
-    console.log("Login/Register page");
+    console.log("Pathname: ", pathname, ", Location: Login/Register page");
     const authRequest = "./routes/auth.js";
     const { generateAuth } = await import(authRequest);
     await generateAuth();
     return;
   }
   if (pathname.toLowerCase().includes("/listing/")) {
-    console.log("Listing spesific page");
+    console.log("Pathname: ", pathname, ", Location: Listing spesific page");
     return;
   }
   if (pathname.toLowerCase().includes("/profile/")) {
-    console.log("Profile page");
+    console.log("Pathname: ", pathname, ", Location: Profile page");
     return;
   }
   if (pathname.toLowerCase().includes("/edit/")) {
-    console.log("Edit/Publish listing page");
+    console.log("Pathname: ", pathname, ", Location: Edit/Publish listing page");
     return;
   } else {
     throw new Error("Pathname is not defined according to the router function. Pathname: ", pathname);
