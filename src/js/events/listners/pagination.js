d289013836660e0ec96490ee47ed1509cf774415
Thea <theola51924@stud.noroff.no>
2024-05-06 22:36:53 +0200

    continued work on the pagination

--- src/js/events/listners/pagination.js
+++ src/js/events/listners/pagination.js
@@ -12,31 +12,21 @@ export function checkNavPagesBtns(currentPage) {
   const nxtBtn = document.getElementById("nxtBtn");
   const prvBtn = document.getElementById("prvBtn");
 
-  console.log("the OG:", currentPage, totalPages);
-
-  if (totalPages > 1 || (totalPages > 1 && currentPage > 1)) {
-    console.log(currentPage);
-    console.log(totalPages);
+  if (totalPages > 1 && currentPage !== totalPages && currentPage !== 1) {
     nxtBtn.style.display = "block";
     prvBtn.style.display = "block";
   }
-  if (totalPages === 1 || (currentPage === 1 && totalPages === 1)) {
-    console.log(currentPage);
-    console.log(totalPages);
-
+  if (totalPages <= 1 && currentPage <= 1) {
     nxtBtn.style.display = "none";
     prvBtn.style.display = "none";
   }
-  if (currentPage === 1) {
-    console.log(currentPage);
-    console.log(totalPages);
-
+  if (currentPage === 1 && totalPages > 1) {
     prvBtn.style.display = "none";
   }
-  if (currentPage !== 1 && totalPages !== 1 && currentPage === totalPages) {
-    console.log(currentPage);
-    console.log(totalPages);
-
+  if (currentPage < 1 && totalPages > 1) {
+    prvBtn.style.display = "block";
+  }
+  if (currentPage === totalPages) {
     nxtBtn.style.display = "none";
   }
 }
