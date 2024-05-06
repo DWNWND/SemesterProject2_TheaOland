579dcb764e299d73ec34a2cc63f706070b69f302
Thea <theola51924@stud.noroff.no>
2024-05-06 19:55:41 +0200

    commented out the bids param and implemented it directly instead

--- src/js/constants/params.js
+++ src/js/constants/params.js
@@ -1,5 +1,5 @@
 export const PARAM_seller = "_seller"; //boolean
-export const PARAM_bids = "?_bids=true"; //boolean
+// export const PARAM_bids = "?_bids=true"; //boolean
 export const PARAM_activeListings = "_active"; //boolean
 export const PARAM_listingTags = "_tag"; //string
 export const PARAM_associatedListing = "_listing";
