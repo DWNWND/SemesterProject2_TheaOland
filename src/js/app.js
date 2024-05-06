6f8dfd983e4f1a182fadff9532d88db0824977af
Thea <theola51924@stud.noroff.no>
2024-05-06 12:07:09 +0200

    working on the path names for the router function to work on the deployed site

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ async function router() {
   const pathname = window.location.pathname;
   console.log(pathname.toLowerCase());
 
-  if (pathname.toLowerCase().includes("/semesterproject2_theaoland/") || pathname === "/") {
+  if (pathname === "/semesterproject2_theaoland/" || pathname === "/") {
     console.log("Home page/Feed");
     const feedRequest = "./routes/feed.js";
     const { generateFeed } = await import(feedRequest);
