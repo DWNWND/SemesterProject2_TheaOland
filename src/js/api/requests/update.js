61c9da2891f3fbd128c14b4678b80e2b3612f08c
Thea <theola51924@stud.noroff.no>
2024-05-08 17:18:07 +0200

    updated the update apiCall function to work with the errorhandling

--- src/js/api/requests/update.js
+++ src/js/api/requests/update.js
@@ -1,38 +1,35 @@
-import { callApiWith } from "../apiCall.mjs";
-import { API_LISTINGS, API_PROFILES } from "../../constants/index.mjs";
+import { callApiWith } from "../apiCall.js";
+import { API_LISTINGS, API_PROFILES } from "../../constants/index.js";
 import { userFeedback } from "../../ui/components/errors/userFeedback.js";
 
 let errorMessage;
 
-export async function updateListing(listing) {
-  try {
-    const errorContainer = document.querySelector("");
+export async function updateListing(listing, listingID) {
+  // const errorContainer = document.querySelector("");
 
-    if (!listing.id) {
-      throw new Error("Update is missing a listingID");
-    }
+  // if (!listing.id) {
+  //   throw new Error("Update is missing a listingID");
+  // }
 
-    const url = API_LISTINGS + `${listing.id}`;
-    const response = await callApiWith(url, {
-      method: "PUT",
-      body: JSON.stringify(listing),
-    });
+  const url = API_LISTINGS + `${listingID}`;
+  const response = await callApiWith(url, {
+    method: "PUT",
+    body: JSON.stringify(listing),
+  });
 
-    if (response.ok) {
-      errorMessage = "The listing was updated";
-      errorContainer.classList.add("success");
-      userFeedback(errorMessage, errorContainer);
-      setTimeout(function () {
-        location.reload();
-      }, 2000);
-    } else {
-      errorMessage = "An unexpected error occured, please try again later";
-      errorContainer.classList.add("error");
-      userFeedback(errorMessage, errorContainer);
-      throw new Error("Couln't update listing");
-    }
-  } catch (error) {
-    console.log(error);
+  if (response.ok) {
+    console.log("updated", response);
+    // errorMessage = "The listing was updated";
+    // errorContainer.classList.add("success");
+    // userFeedback(errorMessage, errorContainer);
+    // setTimeout(function () {
+    //   location.reload();
+    // }, 2000);
+  } else {
+    // errorMessage = "An unexpected error occured, please try again later";
+    // errorContainer.classList.add("error");
+    // userFeedback(errorMessage, errorContainer);
+    throw new Error("Couln't update listing");
   }
 }
 
