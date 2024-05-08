73d6011e5933eb334fab4fa9ae5b3863b12c86a3
Thea <theola51924@stud.noroff.no>
2024-05-08 11:11:43 +0200

    updated the post request for new listing

--- src/js/api/requests/post.js
+++ src/js/api/requests/post.js
@@ -1,31 +1,31 @@
-import { callApiWith } from "../apiCall.mjs";
-import { API_LISTINGS } from "../../constants/index.mjs";
+import { callApiWith } from "../apiCall.js";
+import { API_LISTINGS } from "../../constants/index.js";
 import { userFeedback } from "../../ui/components/errors/userFeedback.js";
 
-let errorMessage;
+// let errorMessage;
 
 export async function publishListing(listing) {
-  const errorContainer = document.querySelector("");
+  // const errorContainer = document.querySelector("");
 
-  try {
-    const url = API_LISTINGS;
-    const response = await callApiWith(url, {
-      method: "POST",
-      body: JSON.stringify(listing),
-    });
-    if (response.status === 201) {
-      location.reload();
-    }
-    if (response.status === 400) {
-      errorMessage = "You are trying to publish an empty listing.";
-      userFeedback(errorMessage, errorContainer);
-    } else if (response.status >= 401) {
-      errorMessage = "An unexpected error occured, please try again later";
-      userFeedback(errorMessage, errorContainer);
-      throw new Error("Unknown error");
-    }
-  } catch (error) {
-    console.log(error);
+  console.log(listing);
+
+  const url = API_LISTINGS;
+  console.log(url);
+
+  const response = await callApiWith(url, {
+    method: "POST",
+    body: JSON.stringify(listing),
+  });
+  console.log(response);
+
+  if (response.status === 201) {
+    console.log(response.status, "ok?");
+    // location.reload();
+  }
+  if (response.status === 400) {
+    throw new Error("You are trying to publish an empty listing.");
+  } else if (response.status >= 401) {
+    throw new Error("An unexpected error occured, please try again later");
   }
 }
 
@@ -35,7 +35,7 @@ export async function publishListing(listing) {
 // };
 
 export async function publishNewBid(listing, bid) {
-  const errorContainer = document.querySelector("");
+  // const errorContainer = document.querySelector("");
 
   try {
     const url = API_LISTINGS + `${listing.id}/bids`;
@@ -48,11 +48,11 @@ export async function publishNewBid(listing, bid) {
       location.reload();
     }
     if (response.status === 400) {
-      errorMessage = "You are trying to publish an empty bid.";
-      userFeedback(errorMessage, errorContainer);
+      // errorMessage = "You are trying to publish an empty bid.";
+      // userFeedback(errorMessage, errorContainer);
     } else if (response.status >= 401) {
-      errorMessage = "An unexpected error occured, please try again later";
-      userFeedback(errorMessage, errorContainer);
+      // errorMessage = "An unexpected error occured, please try again later";
+      // userFeedback(errorMessage, errorContainer);
       throw new Error("Unknown error");
     }
   } catch (error) {
