78963ce607955e1972e320821820f5b86f739466
Thea <theola51924@stud.noroff.no>
2024-05-09 16:26:22 +0200

    updated the errorhandling in the update request function

--- src/js/api/requests/update.js
+++ src/js/api/requests/update.js
@@ -1,56 +1,78 @@
 import { callApiWith } from "../apiCall.js";
 import { API_LISTINGS, API_PROFILES } from "../../constants/index.js";
 import { userFeedback } from "../../ui/components/errors/userFeedback.js";
-import { back } from "../../templates/profile.js";
-import { load } from "../../storage/load.js";
+import { navigateBack } from "../../templates/profile.js";
 
 export async function updateListing(listing, listingID) {
   const userFeedbackContainer = document.getElementById("userFeedbackContainer");
 
-  if (!listing.id) {
-    throw new Error("Update is missing a listingID");
-  }
-  const url = API_LISTINGS + `${listingID}`;
-  const response = await callApiWith(url, {
-    method: "PUT",
-    body: JSON.stringify(listing),
-  });
-  if (response.ok) {
-    console.log("listing updated", response);
-    setTimeout(function () {
+  try {
+    if (!listing.id) {
+      throw new Error("Update is missing a listingID");
+    }
+    const url = API_LISTINGS + `${listingID}`;
+    const response = await callApiWith(url, {
+      method: "PUT",
+      body: JSON.stringify(listing),
+    });
+    if (response.ok) {
+      userFeedbackContainer.classList.add("text-success");
       userFeedback("listing successfully updated", userFeedbackContainer);
-      const pathname = window.location.pathname;
-      if (pathname.toLowerCase().includes("/semesterproject2_theaoland/")) {
-        location.pathname = "/SemesterProject2_TheaOland/";
-      } else {
-        location.pathname = "/";
-      }
-    }, 2000);
-  } else {
-    throw new Error("Couln't update listing");
+      // const uxElementSecondary = document.getElementById("uxElementSecondary");
+      // uxElementSecondary.innerHTML = `<span id="loader" class="loader"><span class="visually-hidden">Loading...</span></span>`;
+
+      setTimeout(function () {
+        const pathname = window.location.pathname;
+        if (pathname.toLowerCase().includes("/semesterproject2_theaoland/")) {
+          location.pathname = "/SemesterProject2_TheaOland/";
+        } else {
+          location.pathname = "/";
+        }
+      }, 2000);
+    } else {
+      throw new Error("Couln't update listing");
+    }
+  } catch (error) {
+    userFeedbackContainer.classList.add("text-error");
+    userFeedback(error, userFeedbackContainer);
   }
 }
 
 export async function updateProfile(userProfile) {
   const userFeedbackContainer = document.getElementById("userFeedbackContainer");
+  try {
+    if (!userProfile.name) {
+      throw new Error("Update is missing a username.");
+    }
+    const url = API_PROFILES + `${userProfile.name}`;
+    const response = await callApiWith(url, {
+      method: "PUT",
+      body: JSON.stringify(userProfile),
+    });
+    if (response.ok) {
+      const saveBtn = document.getElementById("saveBtn");
+      saveBtn.disabled = true;
 
-  if (!userProfile.name) {
-    throw new Error("Update is missing a username");
-  }
-  const url = API_PROFILES + `${userProfile.name}`;
-  const response = await callApiWith(url, {
-    method: "PUT",
-    body: JSON.stringify(userProfile),
-  });
-  if (response.ok) {
-    const result = await response.json();
-    console.log("profile was updated", response);
-    userFeedback("profile successfully updated", userFeedbackContainer);
-    setTimeout(function () {
-      const profileElement = document.getElementById("profileElement");
-      back(profileElement, result.data);
-    }, 2000);
-  } else {
-    throw new Error("Couln't update userprofile");
+      const result = await response.json();
+      const updatedProfile = result.data;
+
+      userFeedbackContainer.classList.add("text-success");
+      userFeedback("profile successfully updated", userFeedbackContainer);
+
+      const uxElementSecondary = document.getElementById("uxElementSecondary");
+
+      console.log(uxElementSecondary);
+      uxElementSecondary.innerHTML = `<span id="loader" class="loader"><span class="visually-hidden">Loading...</span></span>`;
+
+      setTimeout(function () {
+        const profileElement = document.getElementById("profileElement");
+        navigateBack(profileElement, updatedProfile);
+      }, 2000);
+    } else {
+      throw new Error("Couln't update userprofile");
+    }
+  } catch (error) {
+    userFeedbackContainer.classList.add("text-error");
+    userFeedback(error, userFeedbackContainer);
   }
 }
