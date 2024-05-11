5246a307c1802914a5870f3c1b28d847aff1e8e6
Thea <theola51924@stud.noroff.no>
2024-05-11 16:07:18 +0200

    added a check to see if anyones logged in, in the router function

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,8 @@
 async function router() {
+  const loadRequest = "./storage/load.js";
+  const { load } = await import(loadRequest);
+  const loggedIn = load("profile");
+
   const pathname = window.location.pathname;
 
   if (pathname.toLowerCase() === "/semesterproject2_theaoland/" || pathname === "/") {
@@ -9,43 +13,66 @@ async function router() {
     return;
   }
   if (pathname.toLowerCase().includes("/auth/")) {
-    console.log("Pathname: ", pathname, ", Location: Login/Register page");
-    const authRequest = "./routes/auth.js";
-    const { generateAuth } = await import(authRequest);
-    await generateAuth();
+    if (loggedIn) {
+      location.pathname = "/";
+    }
+    if (!loggedIn) {
+      console.log("Pathname: ", pathname, ", Location: Login/Register page");
+      const authRequest = "./routes/auth.js";
+      const { generateAuth } = await import(authRequest);
+      await generateAuth();
+    }
     return;
   }
   if (pathname.toLowerCase().includes("/listing/")) {
-    console.log("Pathname: ", pathname, ", Location: Listing spesific page");
-    const listingRequest = "./routes/listing.js";
-    const { generateListingSpesific } = await import(listingRequest);
-    await generateListingSpesific();
+    if (!loggedIn) {
+      location.pathname = "/";
+    }
+    if (loggedIn) {
+      console.log("Pathname: ", pathname, ", Location: Listing spesific page");
+      const listingRequest = "./routes/listing.js";
+      const { generateListingSpesific } = await import(listingRequest);
+      await generateListingSpesific();
+    }
     return;
   }
   if (pathname.toLowerCase().includes("/profile/")) {
-    console.log("Pathname: ", pathname, ", Location: Profile page");
-    const profileRequest = "./routes/profile.js";
-    const { generateUserProfile } = await import(profileRequest);
-    await generateUserProfile();
+    if (!loggedIn) {
+      location.pathname = "/";
+    }
+    if (loggedIn) {
+      console.log("Pathname: ", pathname, ", Location: Profile page");
+      const profileRequest = "./routes/profile.js";
+      const { generateUserProfile } = await import(profileRequest);
+      await generateUserProfile();
+    }
     return;
   }
   if (pathname.toLowerCase().includes("/edit/")) {
-    console.log("Pathname: ", pathname, ", Location: Edit/Publish listing page");
-    const editRequest = "./routes/edit.js";
-    const { generateEdit } = await import(editRequest);
-    await generateEdit();
-
+    if (!loggedIn) {
+      location.pathname = "/";
+    }
+    if (loggedIn) {
+      console.log("Pathname: ", pathname, ", Location: Edit/Publish listing page");
+      const editRequest = "./routes/edit.js";
+      const { generateEdit } = await import(editRequest);
+      await generateEdit();
+    }
     return;
   }
   if (pathname.toLowerCase().includes("/alllistings/")) {
-    console.log("Pathname: ", pathname, ", Location: All userSpesific Listings");
-    const allListingsRequest = "./routes/allListings.js";
-    const { generateUserFeed } = await import(allListingsRequest);
-    await generateUserFeed();
-
+    if (!loggedIn) {
+      location.pathname = "/";
+    }
+    if (loggedIn) {
+      console.log("Pathname: ", pathname, ", Location: All userSpesific Listings");
+      const allListingsRequest = "./routes/allListings.js";
+      const { generateUserFeed } = await import(allListingsRequest);
+      await generateUserFeed();
+    }
     return;
   } else {
-    throw new Error("Pathname is not defined in the router function. Pathname: ", pathname);
+    throw new Error("The router function is not able to read pathname. Pathname: ", pathname);
   }
 }
 router();
