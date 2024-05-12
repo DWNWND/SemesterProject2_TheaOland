8233cfd006bde3660cc19fda94431d224cbe539f
Thea <theola51924@stud.noroff.no>
2024-05-12 16:03:03 +0200

    added more of the listings meta info to the listing spesific display

--- src/js/templates/listing.js
+++ src/js/templates/listing.js
@@ -2,7 +2,7 @@ import { generateMediaGallery } from "./media.js";
 import { addCurrentBid, addDeadline, displayBidHistory } from "./bids.js";
 import { listenForNewBid } from "../handlers/listners/_index.js";
 
-export function listingSpecificTemplate({ title, media, bids, id, description, endsAt }) {
+export function listingSpecificTemplate({ title, media, bids, id, description, tags, created, updated, endsAt, seller }) {
   const titleContainer = document.getElementById("auction-item-name");
   titleContainer.innerText = title;
 
@@ -17,4 +17,24 @@ export function listingSpecificTemplate({ title, media, bids, id, description, e
 
   const descriptionContainer = document.getElementById("description");
   descriptionContainer.innerText = description;
+
+  const tagsContainer = document.getElementById("tags");
+  tags.forEach((tag) => {
+    console.log(tag);
+    tagsContainer.append(tag);
+  });
+  const sellerContainer = document.getElementById("seller");
+  sellerContainer.innerText = seller.name;
+
+  const createdDate = new Date(created);
+  const createdDateFormatted = createdDate.toLocaleString();
+
+  const createdContainer = document.getElementById("created");
+  createdContainer.innerText = createdDateFormatted;
+
+  const updatedDate = new Date(updated);
+  const updatedDateFormatted = updatedDate.toLocaleString();
+
+  const updatedContainer = document.getElementById("updated");
+  updatedContainer.innerText = updatedDateFormatted;
 }
