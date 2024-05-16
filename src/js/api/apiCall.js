8b45d971ec4cc09a54d9f953d1a1aab8ae27f5bb
Thea <theola51924@stud.noroff.no>
2024-05-16 16:36:12 +0200

    changing to using .env

--- src/js/api/apiCall.js
+++ src/js/api/apiCall.js
@@ -1,8 +1,8 @@
 import { load } from "../storage/load.js";
 // import "dotenv/config";
 
-// const API_KEY = process.env.API_KEY
-const API_KEY = "d2abe438-b85e-4837-bb83-2678d612f606";
+const API_KEY = process.env.API_KEY;
+// const API_KEY = "d2abe438-b85e-4837-bb83-2678d612f606";
 
 export async function callApiWith(url, options = {}) {
   return fetch(url, {
