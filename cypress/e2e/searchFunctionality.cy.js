e0b59b1503df3e71477142efe18e0ce9f2015a6f
Thea <theola51924@stud.noroff.no>
2024-05-17 16:11:15 +0200

    adde cypress test to check search functionality

---
+++ cypress/e2e/searchFunctionality.cy.js
@@ -0,0 +1,15 @@
+import { testUrl } from "../support/testCredentials.js";
+
+describe("search functionality", () => {
+  beforeEach(() => {
+    cy.visit(testUrl);
+    cy.wait(1000);
+    cy.get("#feed").should("be.visible");
+  });
+  it("shows content related to the search query", () => {
+    cy.get("#searchbar").should("be.visible").type("animal");
+    cy.wait(1000);
+    cy.get("#currentPage").should("contain", "1");
+    cy.get("#listing-title").should("contain", "animal");
+  });
+});
