cb2b51fb40d87627898d4fb18260367af48a14b4
Thea <theola51924@stud.noroff.no>
2024-05-18 12:54:17 +0200

    added userfeedback in the delete listing function

--- src/js/api/requests/delete.js
+++ src/js/api/requests/delete.js
@@ -1,22 +1,32 @@
 import { callApiWith } from "../apiCall.js";
 import { API_LISTINGS } from "../../constants/apiParams.js";
+import { userFeedback } from "../../ui/userFeedback/userFeedback.js";
 
 export async function deleteListing(id) {
+  const feedbackContainerOnAction = document.getElementById("feedbackContainerOnAction");
+  const loaderContainerOnAction = document.getElementById("loaderContainerOnAction");
+
   if (!id) {
     throw new Error("Delete is missing a listingID");
   }
-  const url = API_LISTINGS + `${id}`;
+  const url = API_LISTINGS + `/${id}`;
   const response = await callApiWith(url, {
     method: "DELETE",
   });
   if (response.ok) {
-    removeUrlParameter("key");
-    const pathname = window.location.pathname;
-    if (pathname.toLowerCase().includes("/semesterproject2_theaoland/")) {
-      location.pathname = "/SemesterProject2_TheaOland/";
-    } else {
-      location.pathname = "/";
-    }
+    loaderContainerOnAction.style.display = "none";
+    userFeedback("listing successfully deleted", feedbackContainerOnAction);
+    feedbackContainerOnAction.classList.add("text-grayish-purple");
+
+    setTimeout(function () {
+      removeUrlParameter("key");
+      const pathname = window.location.pathname;
+      if (pathname.toLowerCase().includes("/semesterproject2_theaoland/")) {
+        location.pathname = "/SemesterProject2_TheaOland/";
+      } else {
+        location.pathname = "/";
+      }
+    }, 2000);
   } else {
     throw new Error("Something went wrong when contacting the API");
   }
