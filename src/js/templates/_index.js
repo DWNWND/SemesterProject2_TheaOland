3c86c85c22a01526b040c7a7ba14132f9669b1a3
Thea <theola51924@stud.noroff.no>
2024-05-19 14:40:59 +0200

    split the profile page templates into multiple files and added docs

--- src/js/templates/_index.js
+++ src/js/templates/_index.js
@@ -4,4 +4,6 @@ export * from "./listing.js";
 export * from "./listings.js";
 export * from "./media.js";
 export * from "./nav.js";
-export * from "./profile.js";
+export * from "./userProfile.js";
+export * from "./userBids.js";
+export * from "./userWins.js";
