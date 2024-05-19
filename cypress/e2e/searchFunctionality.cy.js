a0191e7bdb7a6fafe478f6806573e7cc0c13768c
Thea <theola51924@stud.noroff.no>
2024-05-19 20:07:40 +0200

    added an extra long wait on the search test, as it just need to load

--- cypress/e2e/searchFunctionality.cy.js
+++ cypress/e2e/searchFunctionality.cy.js
@@ -8,7 +8,7 @@ describe("search functionality", () => {
   });
   it("shows content related to the search query", () => {
     cy.get("#searchbar").should("be.visible").type("cypress");
-    cy.wait(1000);
+    cy.wait(2000);
     cy.get("#currentPage").should("contain", "1");
     cy.get("#listing-title").should("contain", "cypress");
   });
