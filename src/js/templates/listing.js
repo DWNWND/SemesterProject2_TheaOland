7571f9c2d83582d551db57c6eeccf93a36e027de
Thea <theola51924@stud.noroff.no>
2024-05-19 14:42:00 +0200

    added docs

--- src/js/templates/listing.js
+++ src/js/templates/listing.js
@@ -2,6 +2,29 @@ import { generateMediaGallery } from "./media.js";
 import { addCurrentBid, addDeadline, displayBidHistory, addPlaceBidForm } from "./bids.js";
 import { listenForNewBid } from "../handlers/listners/_index.js";
 
+/**
+ * The function populates the listing spesific page with the listing passed to it.
+ *
+ * @param {Object} listingData The listingdata object
+ * @param {string} listingData.title Listing title
+ * @param {Object[]} listingData.media Listing media
+ * @param {Object[]} listingData.bids Listing bids array
+ * @param {string} listingData.id Listing id
+ * @param {string} listingData.description Listing description
+ * @param {Object[]} listingData.tags Listing tags
+ * @param {Date} listingData.created Listing date/time creted
+ * @param {Date} listingData.updated Listing date/time updated
+ * @param {Date} listingData.endsAt Listing deadline
+ * @param {string} listingData.seller Listing seller
+ *
+ * @uses generateMediaGallery To display the listings media gallery¨
+ * @uses addCurrentBid To display the listings current bid
+ * @uses displayBidHistory To display the listings bid history
+ * @uses addPlaceBidForm To generate a form to place new bid
+ * @uses listenForNewBid To listen for a new bid
+ * @uses addDeadline To display the listings deadline countdown
+ */
+
 export function listingSpecificTemplate({ title, media, bids, id, description, tags, created, updated, endsAt, seller }) {
   const titleContainer = document.getElementById("auction-item-name");
   titleContainer.innerText = title;
