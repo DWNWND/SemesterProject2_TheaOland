709cb214cd234b496ed749c376763ee7d1da8fb6
Thea <theola51924@stud.noroff.no>
2024-05-19 19:43:35 +0200

    removed the env. as i cant get it to work

--- src/js/constants/apiParams.js
+++ src/js/constants/apiParams.js
@@ -3,4 +3,4 @@ export const API_REGISTER = "/auth/register";
 export const API_LOGIN = "/auth/login";
 export const API_LISTINGS = API_BASE + `/auction/listings`;
 export const API_PROFILES = API_BASE + `/auction/profiles/`;
-export const API_KEY = process.env.API_KEY;
+export const API_KEY = "d2abe438-b85e-4837-bb83-2678d612f606";
